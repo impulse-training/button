fcc9874203cbf54546b1bbb5032c8b114a9e0ed4
Michael Dawson <m.dawson@y7mail.com>
2023-04-11 16:33:56 -0500

    Update output file name

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default [
     input: "src/index.ts",
     output: {
       name: "Impulse",
-      file: "dist/impulse.js",
+      file: "dist/impulse.js.txt",
     },
     plugins: [
       resolve(),
